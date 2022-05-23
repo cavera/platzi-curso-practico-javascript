fa352b378e1aee61333b7e043c896ead03dbcf15
cavera <cavera.de@gmail.com>
2022-05-23 15:33:07 -0500

    calculo de perimetros y áreas de figuras geometricas

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # platzi-curso-practico-javascript
+
+## Taller 1: Figuras geométricas
+
+-   Paso 1: Definir las formulas
+-   Paso 2: Implementar las fórmulas en Javascript
+-   Paso 3: Crear funciones
+-   Paso 4: Integrar Javascript con HTML
---
+++ figuras.js
@@ -0,0 +1,91 @@
+// código del cuadrado
+console.group("Cuadrado");
+
+function perimetroCuadrado(lado) {
+	return lado * 4;
+}
+
+function areaCuadrado(lado) {
+	return lado * lado;
+}
+
+console.groupEnd();
+// código del triangulo
+console.group("Triangulos");
+
+function perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo) {
+	return ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+}
+
+function areaTriangulo(baseTriangulo, alturaTriangulo) {
+	return (baseTriangulo * alturaTriangulo) / 2;
+}
+
+console.groupEnd();
+
+// código del círculo
+console.group("Circulo");
+
+function diametroCirculo(radio) {
+	return radio * 2;
+}
+const PI = Math.PI;
+
+function perimetroCirculo(radio) {
+	const diametro = diametroCirculo(radio);
+	return diametro * PI;
+}
+
+function areaCirculo(radio) {
+	return radio * radio * PI;
+}
+
+console.groupEnd();
+
+// Interaccion con el HTML
+// cuadrado
+function calcularPerimetroCuadrado() {
+	const input = document.getElementById("InputCuadrado");
+	const value = input.value;
+
+	const perimetro = perimetroCuadrado(value);
+	alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+	const input = document.getElementById("InputCuadrado");
+	const value = input.value;
+
+	const area = areaCuadrado(value);
+	alert(area);
+}
+// triángulo
+function calcularPerimetroTriangulo() {
+	const input1 = document.getElementById("InputTriangulo1");
+	const input2 = document.getElementById("InputTriangulo2");
+	const input3 = document.getElementById("InputTriangulo3");
+
+	const lado1 = Number(input1.value);
+	const lado2 = Number(input2.value);
+	const base = Number(input3.value);
+
+	const perimetro = perimetroTriangulo(lado1, lado2, base);
+	alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+	const input1 = document.getElementById("InputTriangulo1");
+	const input2 = document.getElementById("InputTriangulo2");
+	const input3 = document.getElementById("InputTriangulo3");
+
+	const lado1 = input1.value;
+	const lado2 = input2.value;
+	const base = input3.value;
+
+	const cateto = base / 2;
+
+	const altura = Math.sqrt(lado1 ** 2 - cateto ** 2);
+
+	const area = areaTriangulo(base, altura);
+	alert(area);
+}
